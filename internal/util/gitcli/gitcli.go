4dee69d282822fd62261b6094748b97f077db4f9
Tai <letientai299@gmail.com>
2026-01-22 13:41:16 +0700

    fix(gitcli): enable handling of submodules with `EnableDotGitCommonDir`

--- internal/util/gitcli/gitcli.go
+++ internal/util/gitcli/gitcli.go
@@ -39,7 +39,8 @@ func Open() (*git.Repository, error) {
 	}
 
 	return git.PlainOpenWithOptions(wd, &git.PlainOpenOptions{
-		DetectDotGit: true,
+		DetectDotGit:          true,
+		EnableDotGitCommonDir: true,
 	})
 }
 
