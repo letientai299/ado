acc3d38fbbd1da5c87b853a14ffb9b3a2e919724
Tai <letientai299@gmail.com>
2026-02-07 00:52:49 +0700

    refactor: `pr` is just `pr`, no one would use its other names

--- internal/commands/pull_request/pull_request.go
+++ internal/commands/pull_request/pull_request.go
@@ -6,15 +6,14 @@ import (
 	"github.com/spf13/cobra"
 )
 
-//go:embed pull_request.md
+//go:embed pr.md
 var doc string
 
 func Cmd() *cobra.Command {
 	cmd := &cobra.Command{
-		Use:     "pull-request",
-		Aliases: []string{"pr", "pull"},
-		Short:   "List, view, create or manipulate pull requests",
-		Long:    doc,
+		Use:   "pr",
+		Short: "List, view, create or manipulate pull requests",
+		Long:  doc,
 	}
 	cmd.AddCommand(
 		listCmd(),
