108508bb7e5f59f805b5a522371e20a1e23f3e2a
Bao Xuan <baoxbui87@gmail.com>
2026-03-04 19:00:51 +0700

    feat: add wi create

--- internal/rest/verbs.go
+++ internal/rest/verbs.go
@@ -71,6 +71,30 @@ func httpPut[T any](
 	return httpX[T](ctx, c, http.MethodPut, url, body, qs...)
 }
 
+// httpPatchJsonPatch sends a PATCH request with Content-Type: application/json-patch+json.
+// Required by the Work Item Create/Update APIs.
+func httpPatchJsonPatch[T any](
+	ctx context.Context,
+	c Client,
+	url string,
+	body any,
+	qs ..._shared.Querier,
+) (*T, error) {
+	bodyReader, err := prepareBody(body)
+	if err != nil {
+		return nil, err
+	}
+
+	url = buildFullURL(ctx, url, qs)
+	req, err := http.NewRequestWithContext(ctx, http.MethodPatch, url, bodyReader)
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Set("Content-Type", "application/json-patch+json")
+	return call[T](c, req)
+}
+
 func httpX[T any](
 	ctx context.Context,
 	c Client,
